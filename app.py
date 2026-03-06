569c066c19559d2395a134af80d876a24b5dcce7
Samradnyee03 <samradnyee03@gmail.com>
2026-03-07 00:07:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load ML model
+model = joblib.load("tomato_model.pkl")
+
+labels = {
+0:"Fresh Tomato",
+1:"Spoiling Tomato",
+2:"Spoiled Tomato"
+}
+
+@app.route("/")
+def home():
+    return "Tomato Spoilage Detection API Running"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.json
+
+    temp = data["temp"]
+    humidity = data["humidity"]
+    gas = data["gas"]
+
+    sample = pd.DataFrame([[temp,humidity,gas]],
+    columns=["temp","humidity","gas"])
+
+    prediction = model.predict(sample)[0]
+
+    return jsonify({
+        "status": labels[prediction]
+    })
+
+if __name__ == "__main__":
+    app.run()
